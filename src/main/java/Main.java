936d28e2723caeae08b0563d42cca2c556a472cf
masatoho <tobias@masatoho.com>
2020-06-02 10:19:49 +0200

    Initial Commit. First try to commit to github.

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+  public static void main(String[] args) {
+    String helloWord = "Hello World from masato holding GmbH";
+    int age = 39;
+    double height = 1.83;
+    System.out.println(helloWord);
+  }
+
+  void method() {
+
+  }
+
+  void method2() {
+
+  }
+
+  void method3() {
+
+  }
+
+}
